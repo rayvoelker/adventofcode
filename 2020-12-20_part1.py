a329d69db47925ee3976c4deb599a363921ec64a
Ray Voelker <ray.voelker@gmail.com>
2020-12-20 18:45:09 -0500

    2020-12-20 part 1 ... done!

--- 2020-12-20_part1.py
+++ 2020-12-20_part1.py
@@ -1,3 +1,4 @@
+from collections import deque
 # print dem trees!
 from PIL import Image, ImageDraw
 
@@ -26,11 +27,30 @@ with open("2020-12-20_input.txt") as input:
 class Tile:
     def __init__(self, tile_id, input_tile):
         self.tile_id = tile_id
-        self.up = input_tile[0]
-        self.down = input_tile[-1]
-        self.left = [value[0] for value in input_tile]
-        self.right = [value[-1] for value in input_tile]
+        self.up = list(input_tile[0])
+        self.up_mirror = self.up.copy()
+        self.up_mirror.reverse()
+        self.up_match = []
+
+        self.down = list(input_tile[-1])
+        self.down_mirror = self.down.copy()
+        self.down_mirror.reverse()
+        self.down_match = []
+
+        self.left = list([value[0] for value in input_tile])
+        self.left_mirror = self.left.copy()
+        self.left_mirror.reverse()
+        self.left_match = []
+
+        self.right = list([value[-1] for value in input_tile])
+        self.right_mirror = self.right.copy()
+        self.right_mirror.reverse()
+        self.right_match = []
+
         self.tile = input_tile
+
+        self.match = []
+
 
     def gen_image(self):
         output_image = Image.new('RGBA', (1000,1000))
@@ -55,9 +75,109 @@ for tile in tiles:
     t = Tile(tile, tiles[tile])
     tiles_data[tile] = t
     
-tiles_data[1171].gen_image()
+# for tile in tiles_data:
+#     tiles_data[tile].gen_image()
+
+
+# compare each tile to the other tiles looking for matches 
+for tile in tiles_data:
+    for tile_compare in tiles_data:
+        # don't compare the tile to itself
+        if tile_compare != tile:
+            compare_list = [
+                tiles_data[tile].up == tiles_data[tile_compare].up,
+                tiles_data[tile].up == tiles_data[tile_compare].down,
+                tiles_data[tile].up == tiles_data[tile_compare].left,
+                tiles_data[tile].up == tiles_data[tile_compare].right,
+                tiles_data[tile].up == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].up == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].up == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].up == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].down == tiles_data[tile_compare].up,
+                tiles_data[tile].down == tiles_data[tile_compare].down,
+                tiles_data[tile].down == tiles_data[tile_compare].left,
+                tiles_data[tile].down == tiles_data[tile_compare].right,
+                tiles_data[tile].down == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].down == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].down == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].down == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].left == tiles_data[tile_compare].up,
+                tiles_data[tile].left == tiles_data[tile_compare].down,
+                tiles_data[tile].left == tiles_data[tile_compare].left,
+                tiles_data[tile].left == tiles_data[tile_compare].right,
+                tiles_data[tile].left == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].left == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].left == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].left == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].right == tiles_data[tile_compare].up,
+                tiles_data[tile].right == tiles_data[tile_compare].down,
+                tiles_data[tile].right == tiles_data[tile_compare].left,
+                tiles_data[tile].right == tiles_data[tile_compare].right,
+                tiles_data[tile].right == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].right == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].right == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].right == tiles_data[tile_compare].right_mirror,
+            ]
+
+            compare_list_mirror = [
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].up,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].down,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].left,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].right,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].up_mirror == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].up,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].down,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].left,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].right,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].down_mirror == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].up,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].down,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].left,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].right,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].left_mirror == tiles_data[tile_compare].right_mirror,
+
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].up,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].down,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].left,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].right,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].up_mirror,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].down_mirror,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].left_mirror,
+                tiles_data[tile].right_mirror == tiles_data[tile_compare].right_mirror,
+            ]
+
+            if (True in compare_list) or (True in compare_list_mirror):
+                tiles_data[tile].match.append(tile_compare)
+            print()
+
+# print()
+
+corners = []
 
 for tile in tiles_data:
-    tiles_data[tile].gen_image()
+    print(f"tile: {tile}\tmatches: {tiles_data[tile].match}")
 
+    if len(tiles_data[tile].match) == 2:
+        corners.append(tile)
 
+print(f"corners: {corners}")
+
+product = 1
+for corner in corners:
+    product = product * corner
+
+print(product)
